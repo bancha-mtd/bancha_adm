d05f6d7f32ce4ae9d9f16293ed599ca999e80e2c
L-Hyun <dkh1001@outlook.kr>
2023-03-14 14:07:53 +0900

    Fix(Rename)
    - dateBegin -> startDate

--- src/components/molecules/list/ExhibitionListItem.svelte
+++ src/components/molecules/list/ExhibitionListItem.svelte
@@ -7,8 +7,8 @@
 	export let title: string = "기획전명";
 	export let partners: string = "참여 공급자";
 	export let items: string = "상품";
-	export let dateBegin: string = "시작일자";
-	export let dateEnd: string = "종료일자";
+	export let startDate: string = "시작일자";
+	export let endDate: string = "종료일자";
 	export let views: string = "조회수";
 	export let sales: string = "판매량";
 </script>
@@ -18,8 +18,8 @@
 	<LinkedText to="/exhibition/{id}" width="30%">{title}</LinkedText>
 	<Text width="10%">{partners}</Text>
 	<Text width="10%">{items}</Text>
-	<Text width="10%">{dateBegin}</Text>
-	<Text width="10%">{dateEnd}</Text>
+	<Text width="10%">{startDate}</Text>
+	<Text width="10%">{endDate}</Text>
 	<Text width="10%">{views}</Text>
 	<Text width="10%">{sales}</Text>
 </ListItemLayout>
