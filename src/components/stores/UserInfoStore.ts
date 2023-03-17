e7d2aa475cb107a2bec62cb227146c7e2a6f1ae3
L-Hyun <dkh1001@outlook.kr>
2023-03-17 12:55:21 +0900

    feat add mangerID Field

--- src/components/stores/UserInfoStore.ts
+++ src/components/stores/UserInfoStore.ts
@@ -2,6 +2,7 @@ import { writable, type Writable } from "svelte/store";
 
 export let email:Writable<string> = writable("이메일");
 export let nickname:Writable<string> = writable("닉네임");
+export let managerId: Writable<number> = writable(-1);
 export let type:Writable<string> = writable("-1");
 export let grantType:Writable<String> = writable("");
 export let accessToken:Writable<string> = writable("");
