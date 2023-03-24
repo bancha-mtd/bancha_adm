cc2c34a2ef7db85e92222cdf10cf8430a5370bab
L-Hyun <dkh1001@outlook.kr>
2023-03-24 15:30:37 +0900

    Feat (Item Search)

--- src/components/organisms/item/ItemList.svelte
+++ src/components/organisms/item/ItemList.svelte
@@ -11,39 +11,48 @@
 	import PageSelector from "../../molecules/list/PageSelector.svelte";
 	import Loading from "../../molecules/loading/Loading.svelte";
 	import Search from "../../molecules/search/Search.svelte";
+	import Select from "../../molecules/search/Select.svelte";
 	import TransparentSelect from "../../molecules/search/TransparentSelect.svelte";
 	import APIs from "../../utils/APIs";
+	import type { SelectType } from "../../utils/Types";
 
-	let sortStandards: object[] = [
-		{ id: 1, name: "등록일순" },
-		{ id: 2, name: "조회수순" },
-		{ id: 3, name: "판매량순" },
+	let searchStandards: SelectType[] = [
+		{ id: 1, name: "상품명", value: "productTitle" },
+		{ id: 2, name: "파트너", value: "partnerName" },
+		{ id: 3, name: "담당자", value: "managerName" },
+		{ id: 4, name: "지역", value: "address" },
+		{ id: 4, name: "상품코드", value: "productId" },
 	];
-	let sortOrder: object[] = [
-		{ id: 1, name: "내림차순" },
-		{ id: 2, name: "오름차순" },
+	let sortStandards: SelectType[] = [
+		{ id: 1, name: "등록일순", value: "new" },
+		{ id: 2, name: "조회수순", value: "views" },
+		{ id: 3, name: "판매량순", value: "sale" },
 	];
-	let sortIsActive: object[] = [
-		{ id: 1, name: "전체" },
-		{ id: 2, name: "판매중" },
-		{ id: 3, name: "숨김" },
+	let sortOrder: SelectType[] = [
+		{ id: 1, name: "내림차순", value: "desc" },
+		{ id: 2, name: "오름차순", value: "asc" },
+	];
+	let sortIsActive: SelectType[] = [
+		{ id: 1, name: "전체", value: null },
+		{ id: 2, name: "판매중", value: true },
+		{ id: 3, name: "숨김", value: false },
 	];
 	let searchQuery: string = "";
-	let selectedStandard: object = sortStandards[0];
-	let selectedOrder: object = sortOrder[0];
-	let selectedIsActive: object = sortIsActive[0];
+	let selectedSearchStandard: SelectType = searchStandards[0];
+	let selectedStandard: SelectType = sortStandards[0];
+	let selectedOrder: SelectType = sortOrder[0];
+	let selectedIsActive: SelectType = sortIsActive[0];
 
 	let curPage: number = 1;
 	let maxPage: number = 20;
 	let rangeMin: number = 1;
-
-	let list = [];
 
 	let addItem = () => {
 		push("/item/new");
 	};
 
 	let loading = true;
+	let list = [];
 
 	onMount(() => {
 		getItems(1);
@@ -51,7 +60,20 @@
 
 	const getItems = (page: number) => {
 		loading = true;
-		APIs.getItem({ pageSize: 10, pageNum: page }).then((res) => {
+
+		let option = { pageSize: 10 };
+		option["pageNum"] = page;
+		if (searchQuery !== "") {
+			option[selectedSearchStandard.value] = searchQuery;
+		}
+		option["sortType"] = selectedStandard.value;
+		option["sortDirection"] = selectedOrder.value;
+		if (selectedIsActive.value !== null) {
+			option["useYn"] = selectedIsActive.value;
+		}
+		console.log(JSON.stringify(option));
+
+		APIs.getItem(option).then((res) => {
 			if (res.status === 200) {
 				list = res.data.content;
 				maxPage = res.data.totalPages;
@@ -62,14 +84,25 @@
 		});
 		curPage = page;
 	};
+
+	const search = () => {
+		getItems(1);
+		curPage = 1;
+		rangeMin = 1;
+	};
 </script>
 
 <SearchLayout>
 	<SpaceBetween gap="20px" alignItems="end">
-		<Search onEnter={() => console.log(searchQuery)} value={searchQuery} />
-		<TransparentSelect lists={sortStandards} selected={selectedStandard} />
-		<TransparentSelect lists={sortOrder} selected={selectedOrder} />
-		<TransparentSelect lists={sortIsActive} selected={selectedIsActive} />
+		<Select
+			lists={searchStandards}
+			bind:selected={selectedSearchStandard}
+			borderRadius={true}
+		/>
+		<Search onEnter={search} bind:value={searchQuery} />
+		<TransparentSelect lists={sortStandards} bind:selected={selectedStandard} />
+		<TransparentSelect lists={sortOrder} bind:selected={selectedOrder} />
+		<TransparentSelect lists={sortIsActive} bind:selected={selectedIsActive} />
 	</SpaceBetween>
 	<GreyBackgroundButton
 		onClick={addItem}
