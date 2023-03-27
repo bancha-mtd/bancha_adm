f166488de486590a182b09957d12de870563c187
L-Hyun <dkh1001@outlook.kr>
2023-03-27 13:42:16 +0900

    Feat Publish point add panel

--- src/components/organisms/point/PointAddPanel.svelte
+++ src/components/organisms/point/PointAddPanel.svelte
@@ -1,22 +1,44 @@
 <script lang="ts">
 	import GreyBackgroundButton from "../../atoms/buttons/GreyBackgroundButton.svelte";
+	import BorderedInput from "../../atoms/inputs/BorderedInput.svelte";
 	import BoldText from "../../atoms/texts/BoldText.svelte";
-	import PanelLayout from "../../layouts/PanelLayout.svelte";
+	import DetailPanelLayout from "../../layouts/DetailPanelLayout.svelte";
 	import SearchLayout from "../../layouts/SearchLayout.svelte";
+	import DetailRow from "../../molecules/detail/DetailRow.svelte";
+	import LabeledBorderedInput from "../../molecules/detail/LabeledBorderedInput.svelte";
 
-	export let addManager: () => void = () => {};
+	let item = {
+		userNickname: "",
+		reason: "",
+		point: "0",
+	};
+
+	export let addPoint: () => void = () => {};
 </script>
 
 <SearchLayout>
 	<BoldText fontSize="18px">개별 포인트 지급</BoldText>
 
 	<GreyBackgroundButton
-		onClick={addManager}
+		onClick={addPoint}
 		width="60px"
 		height="30px"
-		fontSize="16px">추가</GreyBackgroundButton
+		fontSize="16px">지급</GreyBackgroundButton
 	>
 </SearchLayout>
-<PanelLayout>
-	<div>수정예정</div>
-</PanelLayout>
+<DetailPanelLayout>
+	<DetailRow title="닉네임">
+		<BorderedInput type="email" bind:value={item.userNickname} />
+	</DetailRow>
+	<DetailRow title="지급 사유">
+		<BorderedInput bind:value={item.reason} width="300px" />
+	</DetailRow>
+	<DetailRow title="포인트">
+		<LabeledBorderedInput
+			type="number"
+			pre=""
+			post="P"
+			bind:value={item.point}
+		/>
+	</DetailRow>
+</DetailPanelLayout>
