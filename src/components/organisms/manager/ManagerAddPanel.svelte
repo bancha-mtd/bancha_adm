20ca489a522d0015e4aba94b47dbcf9cfecdb49e
L-Hyun <dkh1001@outlook.kr>
2023-03-27 13:32:28 +0900

    Feat Publish manager detail

--- src/components/organisms/manager/ManagerAddPanel.svelte
+++ src/components/organisms/manager/ManagerAddPanel.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
 	import GreyBackgroundButton from "../../atoms/buttons/GreyBackgroundButton.svelte";
+	import BorderedInput from "../../atoms/inputs/BorderedInput.svelte";
 	import BoldText from "../../atoms/texts/BoldText.svelte";
+	import DetailPanelLayout from "../../layouts/DetailPanelLayout.svelte";
 	import PanelLayout from "../../layouts/PanelLayout.svelte";
 	import SearchLayout from "../../layouts/SearchLayout.svelte";
+	import DetailRow from "../../molecules/detail/DetailRow.svelte";
+	import Select from "../../molecules/search/Select.svelte";
+
+	export let itemId;
+	let managerGrade = [
+		{ id: 1, name: "최고 관리자", value: 1 },
+		{ id: 2, name: "중간 관리자", value: 2 },
+		{ id: 3, name: "일반 관리자", value: 3 },
+	];
+	let item = {
+		id: itemId,
+		email: "",
+		password: "",
+		nickname: "",
+		grade: managerGrade[0],
+		group: "",
+	};
 
 	export let addManager: () => void = () => {};
 </script>
@@ -17,6 +36,33 @@
 		fontSize="16px">추가</GreyBackgroundButton
 	>
 </SearchLayout>
-<PanelLayout>
-	<div>수정예정</div>
-</PanelLayout>
+<DetailPanelLayout>
+	<DetailRow title="회원번호" head={true}>
+		<BorderedInput
+			height="30px"
+			fontSize="16px"
+			disabled={true}
+			value={item.id.toLocaleString()}
+		/>
+	</DetailRow>
+	<DetailRow title="이메일">
+		<BorderedInput type="email" bind:value={item.email} width="300px" />
+	</DetailRow>
+	<DetailRow title="비밀번호">
+		<BorderedInput type="password" bind:value={item.password} width="300px" />
+	</DetailRow>
+	<DetailRow title="닉네임">
+		<BorderedInput bind:value={item.nickname} width="300px" />
+	</DetailRow>
+	<DetailRow title="권한">
+		<Select
+			lists={managerGrade}
+			bind:selected={item.grade}
+			fontSize="16px"
+			height="30px"
+		/>
+	</DetailRow>
+	<DetailRow title="소속">
+		<BorderedInput bind:value={item.group} width="300px" />
+	</DetailRow>
+</DetailPanelLayout>
--- src/components/organisms/manager/ManagerList.svelte
+++ src/components/organisms/manager/ManagerList.svelte
@@ -7,13 +7,18 @@
 	import ManagerListItem from "../../molecules/list/ManagerListItem.svelte";
 	import PageSelector from "../../molecules/list/PageSelector.svelte";
 	import Search from "../../molecules/search/Search.svelte";
+	import ManagerAddPanel from "./ManagerAddPanel.svelte";
 
 	let searchQuery: string = "";
 
 	let curPage: number = 1;
 	let maxPage: number = 20;
 	let rangeMin: number = 1;
+
+	let selected = 0;
 </script>
+
+<ManagerAddPanel bind:itemId={selected} />
 
 <SearchLayout>
 	<SpaceBetween gap="20px" alignItems="end">
