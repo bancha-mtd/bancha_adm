3991228126c6b4a077cb46ecc7a25e95670f5d3f
L-Hyun <dkh1001@outlook.kr>
2023-03-29 15:49:52 +0900

    Feat Publish Refund Detail

---
+++ src/components/organisms/refund/RefundDetail.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import GreyBackgroundButton from "../../atoms/buttons/GreyBackgroundButton.svelte";
+	import DetailRow from "../../molecules/detail/DetailRow.svelte";
+	import SpaceEnd from "../../atoms/layouts/SpaceEnd.svelte";
+	import DetailPanelLayout from "../../layouts/DetailPanelLayout.svelte";
+	import BorderedInput from "../../atoms/inputs/BorderedInput.svelte";
+
+	export let itemId: string;
+
+	let item = {
+		id: -1,
+		title: "",
+		refundImg: "",
+	};
+
+	const modifyItem = () => {
+		console.log(item);
+		return;
+	};
+</script>
+
+<SpaceEnd gap="10px" marginBottom="20px">
+	<div>{itemId}</div>
+	<GreyBackgroundButton
+		width="50px"
+		height="30px"
+		fontSize="16px"
+		onClick={modifyItem}>수정</GreyBackgroundButton
+	>
+</SpaceEnd>
+<DetailPanelLayout height="calc(100vh - 190px)">
+	<DetailRow head={true} title="코드">
+		<BorderedInput
+			height="30px"
+			fontSize="16px"
+			disabled={true}
+			value={item.id.toLocaleString()}
+		/>
+	</DetailRow>
+	<DetailRow title="제목">
+		<BorderedInput fontSize="16px" width="80%" bind:value={item.title} />
+	</DetailRow>
+	<DetailRow title="이미지">
+		<BorderedInput
+			fontSize="16px"
+			width="500px"
+			type="file"
+			bind:value={item.refundImg}
+		/>
+	</DetailRow>
+</DetailPanelLayout>
--- src/components/pages/setting/RefundDetailPage.svelte
+++ src/components/pages/setting/RefundDetailPage.svelte
@@ -2,6 +2,7 @@
 	import MainLayout from "../../layouts/MainLayout.svelte";
 	import PageLayout from "../../layouts/PageLayout.svelte";
 	import PageHeader from "../../molecules/header/PageHeader.svelte";
+	import RefundDetail from "../../organisms/refund/RefundDetail.svelte";
 
 	export let params: any;
 </script>
@@ -9,7 +10,6 @@
 <MainLayout>
 	<PageHeader title="환불규정" />
 	<PageLayout>
-		<div>수정예정</div>
-		<div>{params.id}</div>
+		<RefundDetail itemId={params.id} />
 	</PageLayout>
 </MainLayout>
