ee4ce3ad77bf88dc0379ccec7763f3a2e9743f55
LeeJunHyeong <windowsjhl@naver.com>
2023-10-04 16:36:38 +0900

    예약 리스트 -> 취소인경우 글자 색상 변경

--- src/components/molecules/listitem/ReservationListItem.tsx
+++ src/components/molecules/listitem/ReservationListItem.tsx
@@ -10,26 +10,26 @@ type Props = {
 const ReservationListItem = ({ reservation }: Props) => {
   return (
     <Flex height="50px">
-      <Text width="5%">
+      <Text width="5%" color={reservation.isCancel ? 'orange' : 'black'}>
         <Link to={`/adm/reservation/${reservation.id}`}>{reservation.id}</Link>
       </Text>
-      <Text width="15%">
+      <Text width="15%" color={reservation.isCancel ? 'orange' : 'black'}>
         <Link to={`/adm/reservation/${reservation.id}`}>
           {reservation.reservationNum}
         </Link>
       </Text>
       {/* <Text width="10%">{reservation.createTime.substr(0, 9)}</Text> */}
-      <Text width="10%">
+      <Text width="10%" color={reservation.isCancel ? 'orange' : 'black'}>
         {reservation.paidAt.replace("T", " ").split(".")[0]}
       </Text>
-      <Text width="5%">{reservation.isCancel ? "취소" : "결제"}</Text>
-      <Text width="10%">{reservation.reservationDate}</Text>
+      <Text width="5%" color={reservation.isCancel ? 'orange' : 'black'}>{reservation.isCancel ? "취소" : "결제"}</Text>
+      <Text width="10%" color={reservation.isCancel ? 'orange' : 'black'}>{reservation.reservationDate}</Text>
       <Flex direction="column" gap="10px" width="15%">
-        <Text>{reservation.memberNickName}</Text>
-        <Text color="grey">{reservation.phoneNumber}</Text>
+        <Text color={reservation.isCancel ? 'orange' : 'black'}>{reservation.memberNickName}</Text>
+        <Text color={reservation.isCancel ? 'orange' : 'grey'}>{reservation.phoneNumber}</Text>
       </Flex>
-      <Text width="20%">{reservation.productTitle}</Text>
-      <Text width="10%">
+      <Text width="20%" color={reservation.isCancel ? 'orange' : 'black'}>{reservation.productTitle}</Text>
+      <Text width="10%" color={reservation.isCancel ? 'orange' : 'black'}>
         {typeof reservation.amount === "number"
           ? reservation.amount.toLocaleString()
           : reservation.amount}
